57ff2513197ee5282a0757201173ea85aa641ffd
Valentin Mari <valentinmari@hotmail.com>
2017-05-02 14:37:37 -0300

    Add sample for configuration

---
+++ src/config.sample.php
@@ -0,0 +1,12 @@
+<?php
+/*
+define('MAILER_SUBJECT', 'Nueva consulta desde ubykuo.com');
+define('MAILER_TO_MAIL', 'info@ubykuo.com');
+define('MAILER_TO_NAME', 'ubykuo');
+
+define('SMTP_HOST', 'relay-hosting.secureserver.net');
+define('SMTP_PORT', '25');
+define('SMTP_SECURITY', null);
+define('SMTP_USER', ''); //Gmail user@gmail.com
+define('SMTP_PASS', ''); //Gmail password
+*/
