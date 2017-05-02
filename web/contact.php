7b4cc9f64847dbf003fdc4e13234b3cb3be6218d
Valentin Mari <valentinmari@hotmail.com>
2017-05-02 14:21:14 -0300

    orden

---
+++ web/contact.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of ubykuo site
+ * @author Valentin Mari <valen.mari@ubykuo.com>
+ * @desc Front controller
+ * 21/04/2017
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+require __DIR__ . '/../src/config.php';
+
+use ubykuo\Contact;
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+
+$contact = new Contact();
+$response = $contact->handle($request);
+header("Location: /");
