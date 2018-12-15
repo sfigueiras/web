0908171a82fff822b5d2b9e4fc32277ea3cdbc01
Santiago Figueiras <santi@ubykuo.com>
2018-12-15 17:29:56 -0300

    * Added font families bold and regular * Landing WIP

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
+    <landing></landing>
   </div>
 </template>
 
 <script>
+  import Landing from '@/components/Landing'
 
-export default {
-  name: 'app',
-  components: {
+  export default {
+    name: 'app',
+    components: {
+      Landing
+    }
   }
-}
 </script>
 
 <style lang="scss">
-@import '~@/assets/scss/_variables';
+  @import '~@/assets/scss/_variables';
+
+  @font-face {
+    font-family: Gilroy-Bold;
+    src: url('~/assets/fonts/Gilroy-Bold.woff');
+  }
+
+  * {
+    font-family: Gilroy-Bold, 'sans-serif';
+  }
+
+  html {
+    height: 100%;
+  }
+  body {
+    position: relative;
+    min-height: 100%;
+    margin: 0;
+  }
+
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="landing">
+    <h1>The right blend of experience hard work, and fun.</h1>
+    <button @click="onClick">tell us your story</button>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Landing'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/assets/scss/_variables';
+
+  section {
+    background-color: $purple-uby;
+    min-height: 100%;
+    width: 100%;
+    display: absolute;
+    color: white;
+  }
+</style>
