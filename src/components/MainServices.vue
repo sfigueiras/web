a4b2fcb711dd63f640a8da79f121bce8692c9dfb
Valentin Mari <valen.mari@ubykuo.com>
2018-12-24 16:49:34 -0300

    MainServices animation on hover

--- src/components/MainServices.vue
+++ src/components/MainServices.vue
@@ -4,34 +4,42 @@
       <h3>Our main services</h3>
       <p class="lead">go trought the code, get to the heart</p>
     </div>
-    <div class="container-fluid main-services">
+    <div class="container main-services">
       <div class="row">
         <div class="col-xs-12 col-sm-3">
           <div class="card">
-            <img src="~@/assets/images/services-robot.png" alt="factory goes digital">
-            <h5>Software evolution</h5>
-            <p>We take over your platform introducing new features and improving existing ones.</p>
+            <div class="card-content">
+              <img src="~@/assets/images/services-robot.png" alt="factory goes digital">
+              <h5>Software evolution</h5>
+              <p>We take over your platform introducing new features and improving existing ones.</p>
+            </div>
           </div>
         </div>
         <div class="col-xs-12 col-sm-3">
           <div class="card">
-            <img src="~@/assets/images/services-applab.png" alt="factory goes digital">
-            <h5>Web platform lab</h5>
-            <p>We can build custom digital products or services to fit your needs.</p>
+            <div class="card-content">
+              <img src="~@/assets/images/services-applab.png" alt="factory goes digital">
+              <h5>Web platform lab</h5>
+              <p>We can build custom digital products or services to fit your needs.</p>
+            </div>
           </div>
         </div>
         <div class="col-xs-12 col-sm-3">
           <div class="card">
-            <img src="~@/assets/images/services-gomobile.png" alt="factory goes digital">
-            <h5>Go mobile</h5>
-            <p>Responsive Progressive Web Apps and Native apps development.</p>
+            <div class="card-content">
+              <img src="~@/assets/images/services-gomobile.png" alt="factory goes digital">
+              <h5>Go mobile</h5>
+              <p>Responsive Progressive Web Apps and Native apps development.</p>
+            </div>
           </div>
         </div>
         <div class="col-xs-12 col-sm-3">
           <div class="card">
-            <img src="~@/assets/images/services-factorygoesdigital.png" alt="factory goes digital">
-            <h5>Factory goes digital</h5>
-            <p>Get real time state of your factory and manage it from any device.</p>
+            <div class="card-content">
+              <img src="~@/assets/images/services-factorygoesdigital.png" alt="factory goes digital">
+              <h5>Factory goes digital</h5>
+              <p>Get real time state of your factory and manage it from any device.</p>
+            </div>
           </div>
         </div>
       </div>
@@ -60,6 +68,7 @@
   .card {
     text-align: center;
     padding: 220px 40px 45px 40px;
+    height: 400px;
     position: relative;
 
     img {
@@ -78,13 +87,18 @@
 
   @media (min-width: 1024px) {
     .main-services {
-      padding: 130px 0;
+      padding: 100px 0;
     }
 
     .card {
       background: #f8f8fa;
       border-radius: $border-radius;
       border: 1px solid #d9d8e0;
+      transition: all 400ms;
+
+      &:hover {
+        transform: scale(1.2);
+      }
     }
   }
 
